0bfae431e8804d0d593d7e3bf487a41374c51a7c
seohae <seohae0406@gmail.com>
2021-09-11 10:59:48 +0900

    BatchConfigurer 인터페이스

--- chapter05/src/main/java/com/example/chapter05/Chapter05Application.java
+++ chapter05/src/main/java/com/example/chapter05/Chapter05Application.java
@@ -1,8 +1,12 @@
 package com.example.chapter05;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* @EnableBatchProcessing 애노테이션을 적용하면 스프링 배치는
+BatchConfigurer 인터페이스를 사용해 프레임워크에서 사용되는 각 인프라스트럭처 컴포넌트의 인스턴스를 얻는다. */
+@EnableBatchProcessing /* 배치 잡에 필요한 인프라스트럭처를 제공 */
 @SpringBootApplication
 public class Chapter05Application {
 
