4cae5a248a0bc6d23406424027d0f20e14d49535
seohae <seohae0406@gmail.com>
2021-09-14 11:05:33 +0900

    TransactionManager

--- chapter05/src/main/java/com/example/chapter05/CustomBatchConfigurer.java
+++ chapter05/src/main/java/com/example/chapter05/CustomBatchConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.repository.support.JobRepositoryFactoryBea
 import org.springframework.batch.support.DatabaseType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -13,6 +14,10 @@ public class CustomBatchConfigurer extends DefaultBatchConfigurer {
     @Autowired
     @Qualifier("repositoryDataSource")
     private DataSource dataSource;
+
+    @Autowired
+    @Qualifier("batchTransactionManager")
+    private PlatformTransactionManager transactionManager;
 
     /**
      * 메서드 재정의
@@ -37,5 +42,22 @@ public class CustomBatchConfigurer extends DefaultBatchConfigurer {
         factoryBean.setDataSource(this.dataSource);
         factoryBean.afterPropertiesSet();
         return factoryBean.getObject();
+    }
+
+    /**
+     * BatchConfigurer.getRansactionManager() 메서드를 호출하면 배치 처리에 사용할 목적으로
+     * 어딘가에 정의해둔 PlatformTransactionManager 가 명시적으로 반환된다.
+     * @return
+     */
+    @Override
+    public PlatformTransactionManager getTransactionManager() {
+        /**
+         * TransactionManager 가 생성되지 않은 경우에는
+         * DefaultBatc hConfigurer 가 기본적으로 setDataSource 수정자 내에서
+         * DataSourceTransactionManager 를 자동으로 생성하기 때문이다.
+         *
+         * TransactionManager 는 BatchCOnfigurer에 의해 이런 방식으로 노출될 수 있는 유일한 컴포넌트다.
+         */
+        return this.transactionManager;
     }
 }
