6be7838bb76bb38958fce7b06c61736a64e8e81b
seohae <seohae0406@gmail.com>
2021-09-19 14:13:17 +0900

    배치 잡 실행시키기

--- chapter06/src/main/java/com/example/chapter06/Chapter06Application.java
+++ chapter06/src/main/java/com/example/chapter06/Chapter06Application.java
@@ -1,13 +1,56 @@
 package com.example.chapter06;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class Chapter06Application {
 
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job job() {
+		return this.jobBuilderFactory.get("job")
+				.incrementer(new RunIdIncrementer())
+				.start(step1())
+				.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return this.stepBuilderFactory.get("step1")
+				.tasklet((stepContribution, chunkContext) -> {
+					System.out.println("step 1 ran today!");
+					return RepeatStatus.FINISHED;
+				}).build();
+	}
+
     public static void main(String[] args) {
-        SpringApplication.run(Chapter06Application.class, args);
+        /** spring.batch.job.enabled : false 설정 */
+        SpringApplication application = new SpringApplication(Chapter06Application.class);
+
+        Properties properties = new Properties();
+        properties.put("spring.batch.job.enabled", false);
+
+        application.setDefaultProperties(properties);
+
+        application.run(args);
     }
 
 }
